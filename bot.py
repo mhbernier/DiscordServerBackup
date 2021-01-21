7e1e06b84456e8061ab92bae45c3725bf463194f
Manoah Bernier <manoahbernier@gmail.com>
2021-01-21 20:42:41 +0100

    Implemented backup_category command

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 
 import json
+import lzma
+import base64
 import datetime
 
 import discord
@@ -56,7 +58,7 @@ async def backup_users(ctx: commands.Context):
     banned_members_count = len(banned_members)
     ts = datetime.datetime.now().timestamp()
 
-    with open(f'{server.name} Users - {ts}.json', 'w', encoding='utf-8') as users_json:
+    with open(f'users - {ts}.json', 'w', encoding='utf-8') as users_json:
 
         for member in server.members:
             if not member.bot:
@@ -86,18 +88,71 @@ async def backup_users(ctx: commands.Context):
 
 
 @bot.command()
-async def backup_channel(ctx: commands.Context):
-    await ctx.send("Command is not implemented yet.")
+async def backup_category(ctx: commands.Context, category_name: str):
 
+    message: discord.Message
+    channel: discord.TextChannel
+    attachment: discord.Attachment
+    category: discord.CategoryChannel
 
-@bot.command()
-async def backup_channels(ctx: commands.Context):
-    await ctx.send("Command is not implemented yet.")
+    server: discord.Guild = ctx.guild
+    messages_count = 0
+    channels_count = 0
+    counter = 1
 
+    for category in server.categories:
+        if category.name.lower() == category_name.lower():
 
-@bot.command()
-async def backup_structure(ctx: commands.Context):
-    await ctx.send("Command is not implemented yet.")
+            channels_count = len(category.text_channels)
+
+            for channel in category.text_channels:
+
+                channel_messages = await channel.history(limit=None).flatten()
+                messages_count += len(channel_messages)
+                ts = datetime.datetime.now().timestamp()
+
+                with open(f'{channel.name} - {ts}.json', 'w+', encoding='utf-8') as messages_json:
+
+                    attachments = list()
+
+                    for message in channel_messages:
+
+                        for attachment in message.attachments:
+                            data = base64.b64encode(await attachment.read()).decode('ascii')
+                            attachments.append({
+                                "id": attachment.id,
+                                "size": attachment.size,
+                                "filename": attachment.filename,
+                                "content": data
+                            })
+
+                        message_json = {
+                            "id": message.id,
+                            "author": message.author.id,
+                            "content": message.clean_content,
+                            "attachments": attachments
+                        }
+
+                        messages_json.write(json.dumps(message_json, sort_keys=True, indent=4))
+
+                    embed = discord.Embed(color=discord.Colour.green())
+                    embed.set_footer(text=f"✅ [{counter}/{channels_count}] {channel.name}")
+                    await ctx.send(embed=embed, delete_after=12.0)
+
+                    counter += 1
+
+            embed = discord.Embed(title=f"{server.name} - {category_name}", color=discord.Colour.blue())
+            embed.add_field(name="Messages", value=str(messages_count), inline=False)
+            embed.add_field(name="Channels", value=str(channels_count), inline=False)
+            embed.set_footer(text=f"✅ Category backed up")
+
+            await ctx.send(embed=embed, delete_after=12.0)
+
+            return
+
+    embed = discord.Embed(color=discord.Colour.red())
+    embed.set_footer(text=f"The category {category_name} doesn't exist")
+    await ctx.send(embed=embed, delete_after=6.0)
 
 
 if __name__ == '__main__':
