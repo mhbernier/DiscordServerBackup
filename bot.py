568139605edcd2406b09ad5a4538e57d285d26b9
Manoah Bernier <manoahbernier@gmail.com>
2021-01-21 14:24:01 +0100

    Updated code structure to actual bot standards

--- bot.py
+++ bot.py
@@ -1,22 +1,41 @@
-import discord
+# This example requires the 'members' privileged intents
 
-bot = discord.Client()
+import discord
+from discord.ext import commands
+
+description = "A Discord bot to backup the content of your server."
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='>', description=description, intents=intents)
 
 
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
+    print(f"Logged in as {bot.user} ({bot.user.id})")
 
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+@bot.command()
+async def backup_users(ctx: commands.Context):
+    await ctx.send("Command is not implemented yet.")
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+
+@bot.command()
+async def backup_channel(ctx: commands.Context):
+    await ctx.send("Command is not implemented yet.")
+
+
+@bot.command()
+async def backup_channels(ctx: commands.Context):
+    await ctx.send("Command is not implemented yet.")
+
+
+@bot.command()
+async def backup_structure(ctx: commands.Context):
+    await ctx.send("Command is not implemented yet.")
 
 
 if __name__ == '__main__':
-    with open('token.txt', 'r') as token:
-        bot.run(token.read())
+    with open('token.txt', 'r') as token_file:
+        bot.run(token_file.read())
